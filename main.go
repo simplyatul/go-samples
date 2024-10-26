3b7e579a307206c7f907ef00f2a49ad098eb67b7
Atul Thosar <atulthosar@gmail.com>
2024-10-26 15:37:19 +0530

    update

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	_ "fmt" // w/o _, compiler will throw an error. Ok to use in development code. Must be avoided in production grade code
-	"go-samples/namaste"
-	"go-samples/pointers"
+	_ "go-samples/namaste"
+	_ "go-samples/pointers"
 
 	"go-samples/structs"
 	"log"
@@ -13,8 +13,9 @@ func main() {
 	// Set log flags to include date, time with milliseconds, short file name, and line number
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
 
-	namaste.SayNamaste()
-	pointers.Checkptr_1()
+	// namaste.SayNamaste()
+	// pointers.Checkptr_1()
 
 	structs.Struct_0()
+
 }
--- structs/structs-0.go
+++ structs/structs-0.go
@@ -4,6 +4,51 @@ import (
 	"log"
 )
 
+type Item struct {
+	itemName        string
+	itemDescription string
+	cost            float32
+}
+
+type localItem struct {
+	itemName        string
+	itemDescription string
+	cost            float32
+}
+
 func Struct_0() {
 	log.Println("----------------------------")
+
+	itPtr := &Item{
+		itemName:        "Pencil",
+		itemDescription: "Pencil",
+		cost:            10.2,
+	}
+	log.Printf("Type of itPtr => %T", itPtr) // Type of itPtr => *structs.Item
+
+	itLoPtr := &localItem{}
+	log.Printf("Type of itLoPtr => %T", itLoPtr) // Type of itLoPtr => *structs.localItem
+
+	anonymousStruct := struct {
+		f1 string
+		f2 int
+	}{
+		f1: "Field 1",
+		f2: 10,
+	}
+	ptrToanonymousStruct := &anonymousStruct
+
+	log.Printf("Type of anonymousStruct => %T", anonymousStruct)           // Type of anonymousStruct => struct { f1 string; f2 int }
+	log.Printf("Type of ptrToanonymousStruct => %T", ptrToanonymousStruct) // Type of ptrToanonymousStruct => *struct { f1 string; f2 int }
+
+	type anonymousFieldStruct struct {
+		string
+		int
+	}
+
+	aFS := anonymousFieldStruct{string: "Name", int: 20}
+	log.Printf("Type of anonymousFieldStruct => %T", aFS) // Type of anonymousFieldStruct => structs.anonymousFieldStruct
+
+	ptrToaFS := &aFS
+	log.Printf("Type of ptrToaSF => %T", ptrToaFS) // Type of ptrToaSF => *structs.anonymousFieldStruct
 }
